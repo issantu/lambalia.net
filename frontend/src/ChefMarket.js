9f54efa4a37a60741abe06f60da5954e35c037e6
issantu <issantu@gmail.com>
2025-09-14 14:07:44 -0500

    Create ChefMarket.js

---
+++ frontend/src/ChefMarket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChefMarket = () => {
+  return (
+    <div>
+      <h1>🎉 IT IS WORKING!</h1>
+      <p>Chef Market component loaded successfully</p>
+    </div>
+  );
+};
+
+export default ChefMarket;
